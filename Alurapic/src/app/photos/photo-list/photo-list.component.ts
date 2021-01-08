ef592768d77884f9cd76017481259ef0a5684749
Adriano Maurmann <adriano.maurmann@ecs.com.br>
2021-01-08 20:44:54 -0300

    curso 1 aula 5

--- Alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ Alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -7,10 +9,11 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit,OnDestroy {
 
   photos: Photo[];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(
     private activatedRoute: ActivatedRoute
@@ -20,5 +23,19 @@ export class PhotoListComponent implements OnInit {
     // le o dado lido pelo resolver
     this.photos = this.activatedRoute.snapshot.data['photos'];
     
+    // tecnica para garantir que a pesquisa seja aplicada apenas apos 300ms que o usuario 
+    // parou de digitar.
+    // recurso conhecido como Lettable operators do RxJS
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter);
   }
+  
+  // para nao gerar memory leak é necessario remover cancelar o subscribe
+  // para subscribers de httlclient nao é necessario pois eles sao automaticamente 
+  // encerrados ao receber a resposta do servidor
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
 }
